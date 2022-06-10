dddc94b632c65de677aef597738417a2cd492b8f
mad <mad@jangjicheol.local>
2022-06-10 17:31:09 +0900

    first layout

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    setInterval(function() {
+        $(".section4 .img img:first-child").delay(3000)
+        $(".section4 .img img:first-child").animate({marginLeft: "-1200px"}, 1500)
+    });
+
+});
+
+
+
